8a2631a997226c2159185833ab192b9f8c85374f
akani28 <nietocevallos61@gmail.com>
2024-03-14 21:06:08 -0500

    commit prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int magicNumber;
+        int guessNumber;
+        Console.Write("What is the magic number? ");
+        magicNumber = int.Parse(Console.ReadLine());
+        do
+        {
+            
+            
+            Console.Write("What is your guess? ");
+            guessNumber = int.Parse(Console.ReadLine());
+
+            if (guessNumber == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+
+            }
+        } while (guessNumber != magicNumber);
+
+
     }
 }
