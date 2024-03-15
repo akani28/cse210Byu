2a0dfbca2d7a0cb1c282a93d1b3266e0949e2144
akani28 <nietocevallos61@gmail.com>
2024-03-14 21:38:05 -0500

    commit prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,31 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        int magicNumber;
+        Random randomNumber = new Random();
+        int magicNumber = randomNumber.Next(1, 101);
         int guessNumber;
-        Console.Write("What is the magic number? ");
-        magicNumber = int.Parse(Console.ReadLine());
+        String response;
+        int count = 0;
+        //Console.Write("What is the magic number? ");
+        //magicNumber = int.Parse(Console.ReadLine());
         do
         {
-            
-            
-            Console.Write("What is your guess? ");
-            guessNumber = int.Parse(Console.ReadLine());
+            do
+            {
 
-            if (guessNumber == magicNumber)
-            {
-                Console.WriteLine("You guessed it!");
-            }
-            else if (guessNumber < magicNumber)
-            {
-                Console.WriteLine("Higher");
-            }
-            else
-            {
-                Console.WriteLine("Lower");
 
-            }
-        } while (guessNumber != magicNumber);
+                Console.Write("What is your guess? ");
+                guessNumber = int.Parse(Console.ReadLine());
+
+                if (guessNumber == magicNumber)
+                {
+                    response = "no";
+                    Console.WriteLine("You guessed it!");
+                }
+                else if (guessNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Lower");
+
+                }
+
+                count++;
+            } while (guessNumber != magicNumber);
+            Console.WriteLine($"you tried {count} times ");
+            Console.WriteLine($"the magic number is {magicNumber} ");
+
+            Console.WriteLine("for continue type yes, for exit type no");
+            response = Console.ReadLine();
+        } while (response == "yes");
+
+
+
 
 
     }
