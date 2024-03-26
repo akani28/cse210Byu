5b99bd25f78cb5f5e043126ca5b48bb0d19c670a
akani28 <nietocevallos61@gmail.com>
2024-03-26 16:20:22 -0500

    update the word in scripture

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -2,12 +2,26 @@ using System;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
+        string dataUser ="";
         Scripture scripture = new Scripture();
         Console.WriteLine("Hello Develop03 World!");
         scripture.ShowScripture();
-        scripture.replaceWord();
-        scripture.replaceWord();
+        
+        while(dataUser!="quit"){
+            Console.WriteLine("Press enter to continue or type 'quit' to finish: ");
+            dataUser = Console.ReadLine();
+            //Console.Clear();
+            scripture.replaceWord();
+           
+
+        }
+
+        
+        
+
+    
     }
 }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -8,8 +8,8 @@ class Scripture
     public Scripture()
     {
         reference = new Reference("3 Nefi", "22", "7");
-        word = new Word("y todos tus hijos seran instruidos por el señor y grande sera la paz de tus hijos ");
-        numberRandom = random.Next(0, word.getWord().Split().Length + 1);
+        word = new Word("y todos tus hijos seran instruidos por el señor y grande sera la paz de tus hijos \n");
+        
 
     }
 
@@ -20,8 +20,10 @@ class Scripture
     }
     public void replaceWord()
     {
+        numberRandom = random.Next(0, word.getWord().Split().Length + 1);
         string[] words = word.getWord().Split(" ");
         string randomWord = words[numberRandom];
+        Console.WriteLine("the randomWord is "+words[numberRandom]);
         string str = new string('_',randomWord.Length);
         string textword = word.getWord().Replace(randomWord,str);
         word.setWord(textword);
