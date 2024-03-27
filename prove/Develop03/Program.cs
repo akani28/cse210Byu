99e975725f42d948f48c6ef45459c8e44a414d49
akani28 <nietocevallos61@gmail.com>
2024-03-27 10:04:00 -0500

    issues the number random is empty

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -7,14 +7,15 @@ class Program
     {
         string dataUser ="";
         Scripture scripture = new Scripture();
-        Console.WriteLine("Hello Develop03 World!");
         scripture.ShowScripture();
         
         while(dataUser!="quit"){
-            Console.WriteLine("Press enter to continue or type 'quit' to finish: ");
+            Console.Clear();
+            scripture.ShowScripture();
+            Console.WriteLine("\nPress enter to continue or type 'quit' to finish: ");
             dataUser = Console.ReadLine();
-            //Console.Clear();
             scripture.replaceWord();
+            
            
 
         }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -9,7 +9,7 @@ class Scripture
     {
         reference = new Reference("3 Nefi", "22", "7");
         word = new Word("y todos tus hijos seran instruidos por el señor y grande sera la paz de tus hijos \n");
-        
+
 
     }
 
@@ -20,18 +20,32 @@ class Scripture
     }
     public void replaceWord()
     {
-        numberRandom = random.Next(0, word.getWord().Split().Length + 1);
+        numberRandom = random.Next(0, word.getWord().Split().Length + 1);//el numero aleatorio no se genera en ocasiones.lo que causa el error
         string[] words = word.getWord().Split(" ");
+        Console.WriteLine($"{numberRandom}");
+        foreach (string word in words)
+        {
+         Console.Write($" {word} ");
+        }
         string randomWord = words[numberRandom];
-        Console.WriteLine("the randomWord is "+words[numberRandom]);
-        string str = new string('_',randomWord.Length);
-        string textword = word.getWord().Replace(randomWord,str);
-        word.setWord(textword);
-        ShowScripture();
+        if (randomWord.Contains("_"))
+        {
+
+        }
+        else
+        {
+            Console.WriteLine("the randomWord is " + words[numberRandom]);
+            string str = new string('_', randomWord.Length);
+            string textword = word.getWord().Replace(randomWord, str);
+            word.setWord(textword);
+            ShowScripture();
+
+        }
+
 
         //foreach (string word in words)
         //{
-           // Console.WriteLine($"{word}");
+        // Console.Write($"{word}");
         //}
 
     }
