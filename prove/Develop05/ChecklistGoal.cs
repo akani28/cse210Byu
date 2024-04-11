e36954a0182b58930b254f904dc49a07a897b26b
akani28 <nietocevallos61@gmail.com>
2024-04-11 02:59:33 -0500

    changes in _target

--- prove/Develop05/ChecklistGoal.cs
+++ prove/Develop05/ChecklistGoal.cs
@@ -1,14 +1,15 @@
-class Checklist:Goals
+class Checklist : Goals
 {
     private int amountCompleted;
-    private int target;
+    private int _target;
     private int bonus;
-    public Checklist(string name, string description, int amount, int target, int bonus): base(name, description, amount)
+    public Checklist(string name, string description, int amount, int target, int bonus) : base(name, description, amount)
     {
+        _target = target;
 
     }
     public override string GetDetailsString()
     {
-        return $"[ ] {base.GetName()} ({base.GetDescription()}) -- Currently completed: {bonus=0}/{target}";
+        return $"[ ] {base.GetName()} ({base.GetDescription()}) -- Currently completed: {bonus = 0}/{_target}";
     }
 }
