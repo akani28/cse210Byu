aaf7289ce7991140e2d1741367cef08de3279f67
akani28 <nietocevallos61@gmail.com>
2024-04-13 05:40:52 -0500

    update

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -1,10 +1,21 @@
 class Order
 {
     private Customer _customer;
-    private List<Product> products;
+    private List<Product> products = new List<Product>();
     public Order(Customer customer)
     {
         _customer = customer;
+
+    }
+
+    public List<Product> GetProducts()
+    {
+        return products;
+    }
+
+    public void SetProducts(Product product)
+    {
+        products.Add(product);
 
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,20 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
+        Customer customer = new Customer();
+        Order order = new Order(customer);
         Product keyboard = new Product("Mechanic logitech", "001", 60.00f);
         Product mouse = new Product("mouse logitech", "002", 20.00f);
         Product laptop = new Product("Asus vivobook", "003", 1500.00f);
         Console.WriteLine("Hello, welcome, we are Akani Dragons company.");
         Console.WriteLine($"{Environment.NewLine}");
         string input = "";
-        while (input != "4")
+        while (input != "5")
         {
 
             Console.WriteLine("Avalible products:");
             Console.WriteLine("  1. keyboard");
             Console.WriteLine("  2. Mouse");
             Console.WriteLine("  3. Laptop");
-            Console.WriteLine("  4.Quit");
+            Console.WriteLine("  4. generate shipping label");
+            Console.WriteLine("  5.Quit");
             Console.Write("What product do you want to carry? ");
             input = Console.ReadLine();
             if (input == "1")
@@ -27,8 +30,54 @@ class Program
                 input = Console.ReadLine();
                 keyboard.SetAmount(int.Parse(input));
                 keyboard.TotalCost();
+                order.SetProducts(keyboard);
 
             }
+            else if (input == "2")
+            {
+                mouse.DetailsProduct();
+                Console.Write($"How many {mouse.GetName()} do you want to carry? ");
+                input = Console.ReadLine();
+                mouse.SetAmount(int.Parse(input));
+                mouse.TotalCost();
+                order.SetProducts(mouse);
+
+            }
+            else if (input == "3")
+            {
+                laptop.DetailsProduct();
+                Console.Write($"How many {laptop.GetName()} do you want to carry? ");
+                input = Console.ReadLine();
+                laptop.SetAmount(int.Parse(input));
+                laptop.TotalCost();
+                order.SetProducts(laptop);
+
+            }
+            //Console.WriteLine("Help me with the following information for the order: ");
+            //Console.Write("you name is: ");
+            //string nameCustomer = Console.ReadLine();
+            //customer.SetName(nameCustomer);
+            //customer.SetAddress();
+
+            if(input == "4")
+            {
+                Console.WriteLine("Help me with the following information for the order: ");
+            Console.Write("you name is: ");
+            string nameCustomer = Console.ReadLine();
+            customer.SetName(nameCustomer);
+            customer.SetAddress();
+                Console.WriteLine($"name: {customer.GetName()}");
+                Console.WriteLine($"address: {customer.GetAddress().ShowAddress()}");
+                float cost = 0;
+                foreach (Product product in order.GetProducts())
+                {
+                    product.DetailsProduct();
+                    cost = cost + product.TotalCost();
+                    
+                }
+                Console.WriteLine($"you must pay: {cost}$");
+            }
+            
 
         }
 
